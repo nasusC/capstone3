2a2645c2c3fc183f64ed2d363d1b044c39cff767
M.Y. Jung <alclssha0301@naver.com>
2024-05-02 01:32:32 +0900

    두번째 커밋 로그인 창;

---
+++ src/main/java/cdproject/capstone3/controller/HomeController.java
@@ -0,0 +1,28 @@
+package cdproject.capstone3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class HomeController {
+
+  @GetMapping("/")
+  public String show_home(){
+    return "index";
+  }
+
+  @GetMapping("login")
+  public String login(){
+    return "login";
+  }
+  @PostMapping("login")
+  public String logincheck(@RequestParam String userId){
+    return null;
+  }
+
+
+}
